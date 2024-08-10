7bb1e58a0041c0f9174920dcc3134eab695c9cda
BlitzBitzCode <171477575+BlitzBitzCode@users.noreply.github.com>
2024-08-10 04:56:17 +0100

    First commit of this file
    Still needs some work doing to it.
    Exception needs to be caught on hashlib.new() for example.

---
+++ file2mac.py
@@ -0,0 +1,48 @@
+import sys
+import hashlib
+
+
+def display_help():
+    print("file2mac.py:")
+    print("python3 fil2mac.py HASHALGORITHM FILENAME")
+    sys.exit()
+
+def display_algorithms():
+    print("displaying algorithms")
+    algorithmCount = len(hashlib.algorithms_available)
+    algorithms = hashlib.algorithms_available
+    count = 0
+    while count < algorithmCount:
+        print(str(count) + "\t - " + algorithms.pop())
+        count += 1
+    sys.exit()
+
+def get_message_digest(algo, target):
+    msgdigest = hashlib.new(algo)
+    f = open(target, 'rb')
+    data = bytes(f.read())
+    f.close()
+    msgdigest.update(data)
+    print(msgdigest.hexdigest())
+    sys.exit()
+
+if __name__ == "__main__":
+    params = sys.argv
+
+    if len(params)== 1:
+        display_help()
+    elif len(params) == 2:
+        param1 = sys.argv[0]
+        param2 = sys.argv[1]        
+        if param2 == "--list":
+            display_algorithms()
+        elif param2 == "--help":
+            display_help()
+        else:
+            sys.exit()
+    elif len(params) == 3:
+        print("run hashing...")
+        print("please wait...")
+        get_message_digest(sys.argv[1], sys.argv[2])
+    else:
+        pass
